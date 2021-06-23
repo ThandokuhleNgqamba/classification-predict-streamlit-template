9db4bfc0f774cf66a271b58a253487fa38c293ca
YsfMnsr <ysfmansoor@gmail.com>
2021-06-23 12:46:34 +0200

    Added more pages and pictures

--- base_app.py
+++ base_app.py
@@ -56,7 +56,7 @@ def main():
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
+	options = ["Prediction", "Information","EDA", "Meet the Team","Contact Us","FAQ"]
 	selection = st.sidebar.selectbox("Choose Option", options)
 
 	# Building out the "Information" page
